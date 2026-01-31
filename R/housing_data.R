77653aea3f18c02226cd5997634050714bf3ecfe
stats-benson <jbenson2470@gmail.com>
2026-01-30 20:46:18 -0800

    added housing data for PB exploration

---
+++ R/housing_data.R
@@ -0,0 +1,12 @@
+#' housing_data Dataset
+#'
+#'  In 2015, we sampled 13 homes that were for sale just north of Lake Macatawa in Michigan, and recorded the sale price (in thousands of dollars) as listed on Zillow (a real estate website).
+#'
+#' @format A data frame with 13 rows and 3 variables:
+#' \describe{
+#'   \item{sqft}{Square footage of house. (Quantitative, rounded to square foot.)}
+#'   \item{price.1000}{Price of house in thousands of dollars. (Quantitative, rounded to hundred dollars. )}
+#'   \item{lake}{Binary categorical variable of if the house is on a lake or not. ("lakefront" or "notlakefront")}
+#' }
+#' @source Tintle, N., Chance, B., Cobb, G., Rossman, A., Roy, S., & VanderStoep, J. (2019). Intermediate Statistical Investigations. John Wiley & Sons. housing prices in Michigan Dataset.
+"housing_data"
